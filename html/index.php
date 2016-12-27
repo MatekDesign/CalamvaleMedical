e3bc61ee777469cb839594fd79362dda6f8e9665
Wrdle <mdagostino175@gmail.com>
2016-12-27 11:51:36 +1000

    Created dynamic system using PHP for footer and resources viewing
    That probs made no sense

---
+++ html/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        
+        <?php include 'resources.php'; ?>
+        
+        <title>Calamvale Medical Center</title>
+    </head>
+    <body>
+        <div>
+            <ul class="navbar">
+                <li><a href="#home">Home</a></li>
+                <li><a href="#news">News</a></li>
+                <li style="float:right"><a href="#contact">Contact</a></li>
+                <li style="float:right"><a class="active" href="#about">About</a></li>
+            </ul>
+        </div>
+        <div class="header">
+            <div class="header-vertical-align">
+                <span class="header-title">HIGH QUALITY, DEPENDABLE HEALTHCARE FOR THE ENTIRE FAMILY</span>
+                <span class="header-sub-title">Come in for a checkup today</span>
+                <form>
+                    <button class="header-button" type="submit">BOOK AN APPOINTMENT</button>
+                </form>
+            </div>
+        </div>    
+        <br>
+       <?php include 'footer.php'; ?> 
+    </body>
+</html>
