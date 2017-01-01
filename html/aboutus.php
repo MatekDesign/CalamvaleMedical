8c3b34bd01e1bda33e5f407edc709ecb7a87d1d8
Wrdle <mdagostino175@gmail.com>
2017-01-01 22:46:25 +1000

    Updated abous us page with working code :p

--- html/aboutus.php
+++ html/aboutus.php
@@ -7,6 +7,7 @@
         <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
         
         <?php include 'resources.php'; ?>
+		<link href="resources/aboutus.css" rel="stylesheet">
         
         <title>Calamvale Medical Center</title>
     </head>
@@ -25,27 +26,29 @@
         <div class="container" style="padding: 50px 50px 50px 50px">
             <div class="row">
                 <h3>Lorem Ipsum</h3>
-                <div class="col-md-4">
-                    <img src="resources/img/person1.jpg" style="border-radius: 360px; width: 250px">
-                </div>
-                <div class="col-md-8">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam accumsan iaculis urna. Nulla vitae risus risus. Morbi accumsan vitae ipsum id auctor. Aenean laoreet accumsan enim, at lacinia nulla sodales ac. Aenean sit amet varius justo. Ut elit nunc, rhoncus at mollis in, finibus varius purus. Curabitur vehicula massa sed maximus fringilla.
+                <div style="content-container">
+                                        <div class="content-img">
+                        <img src="resources/img/lol1.jpg" style="border-radius: 360px; width: 100%;">
+                    </div>
+                    <div class="content-text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam accumsan iaculis urna. Nulla vitae risus risus. Morbi accumsan vitae ipsum id auctor. Aenean laoreet accumsan enim, at lacinia nulla sodales ac. Aenean sit amet varius justo. Ut elit nunc, rhoncus at mollis in, finibus varius purus. Curabitur vehicula massa sed maximus fringilla.
 
-Mauris vel auctor sem. In sed hendrerit mi. Nunc in nisi et quam malesuada rutrum non quis sapien. Duis massa orci, mollis eget ipsum id, tempor imperdiet felis. Aliquam fringilla felis odio, eget egestas nulla vehicula sed. Donec sit amet risus vulputate, faucibus tellus venenatis, laoreet ipsum. Suspendisse potenti. Donec viverra, purus interdum convallis porttitor, enim est sagittis turpis, at ultricies sem enim sit amet ante. Praesent eget sem vestibulum, sagittis justo quis, blandit neque. Aenean eget dolor finibus, consectetur massa sodales, facilisis diam. Donec non nisl at sapien tincidunt aliquet.
+                    Mauris vel auctor sem. In sed hendrerit mi. Nunc in nisi et quam malesuada rutrum non quis sapien. Duis massa orci, mollis eget ipsum id, tempor imperdiet felis. Aliquam fringilla felis odio, eget egestas nulla vehicula sed. Donec sit amet risus vulputate, faucibus tellus venenatis, laoreet ipsum. Suspendisse potenti. Donec viverra, purus interdum convallis porttitor, enim est sagittis turpis, at ultricies sem enim sit amet ante. Praesent eget sem vestibulum, sagittis justo quis, blandit neque. Aenean eget dolor finibus, consectetur massa sodales, facilisis diam. Donec non nisl at sapien tincidunt aliquet.
+                    </div>
                 </div>
             </div>
             <br>
             <div class="row">
                 <h3>Lorem Ipsum</h3>
-                <div class="col-md-8">
-                    <div style="height: 250px; vertical-align: ">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam accumsan iaculis urna. Nulla vitae risus risus. Morbi accumsan vitae ipsum id auctor. Aenean laoreet accumsan enim, at lacinia nulla sodales ac. Aenean sit amet varius justo. Ut elit nunc, rhoncus at mollis in, finibus varius purus. Curabitur vehicula massa sed maximus fringilla.
+                <div style="content-container">
+                    <div class="content-text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam accumsan iaculis urna. Nulla vitae risus risus. Morbi accumsan vitae ipsum id auctor. Aenean laoreet accumsan enim, at lacinia nulla sodales ac. Aenean sit amet varius justo. Ut elit nunc, rhoncus at mollis in, finibus varius purus. Curabitur vehicula massa sed maximus fringilla.
 
-Mauris vel auctor sem. In sed hendrerit mi. Nunc in nisi et quam malesuada rutrum non quis sapien. Duis massa orci, mollis eget ipsum id, tempor imperdiet felis. Aliquam fringilla felis odio, eget egestas nulla vehicula sed. Donec sit amet risus vulputate, faucibus tellus venenatis, laoreet ipsum. Suspendisse potenti. Donec viverra, purus interdum convallis porttitor, enim est sagittis turpis, at ultricies sem enim sit amet ante. Praesent eget sem vestibulum, sagittis justo quis, blandit neque. Aenean eget dolor finibus, consectetur massa sodales, facilisis diam. Donec non nisl at sapien tincidunt aliquet.
+                    Mauris vel auctor sem. In sed hendrerit mi. Nunc in nisi et quam malesuada rutrum non quis sapien. Duis massa orci, mollis eget ipsum id, tempor imperdiet felis. Aliquam fringilla felis odio, eget egestas nulla vehicula sed. Donec sit amet risus vulputate, faucibus tellus venenatis, laoreet ipsum. Suspendisse potenti. Donec viverra, purus interdum convallis porttitor, enim est sagittis turpis, at ultricies sem enim sit amet ante. Praesent eget sem vestibulum, sagittis justo quis, blandit neque. Aenean eget dolor finibus, consectetur massa sodales, facilisis diam. Donec non nisl at sapien tincidunt aliquet.
                     </div>
-                </div>
-                <div class="col-md-4">
-                    <img src="resources/img/person2.png" style="border-radius: 360px;width: 250px;">
+                    <div class="content-img">
+                        <img src="resources/img/lol1.jpg" style="border-radius: 360px; width: 100%;">
+                    </div>
                 </div>
             </div>
         </div>
