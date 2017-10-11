1d5a31b5efe61d1611d5442b043de990cec27f29
Matt <mdagostino175@gmail.com>
2017-10-11 22:26:36 +1000

    I added cards to the index

--- new/html/index.php
+++ new/html/index.php
@@ -32,6 +32,26 @@
     </div>
     <?php include 'navbar.php'; ?>
 
+    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+        <div class="modal-body">
+            ...
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+        </div>
+    </div>
+    </div>
+    
     <!-- Header -->
     <div class="parallax">
         <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
@@ -75,8 +95,7 @@
     </div>
     <script>
         function headerButtonClicked() {
-            var elem = document.getElementById("bookappointment");
-            elem.style.display = "inherit";
+            $('#myModal').modal('toggle');
         }
     </script>
     <br>
