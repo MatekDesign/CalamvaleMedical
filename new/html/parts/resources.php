9ddae1669e7e09fd148fae98465a97239f2cabbb
Wrdle <mdagostino175@gmail.com>
2018-01-22 21:59:17 +1000

    Updated Boostrap Version
    Now on Bootstrap 4.0.0 stable not alpha as we were previously using

--- new/html/parts/resources.php
+++ new/html/parts/resources.php
@@ -1,5 +1,5 @@
 <?php include 'copyright.php'; ?>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
 <!-- Include all compiled plugins (below), or include individual files as needed -->
 <link href="https://fonts.googleapis.com/css?family=Montserrat:400+700|Open+Sans:300,400,600" rel="stylesheet">
